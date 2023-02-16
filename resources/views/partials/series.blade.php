ab8957f231a3fb1a26c0047147592baf4d9f158e
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-02-17 00:59:43 +0100

    series module done

--- resources/views/partials/series.blade.php
+++ resources/views/partials/series.blade.php
@@ -1,17 +1,19 @@
-<section class="series">
-	<div class="container">
+<section class="series my-bg-dark">
+	<div class="container py-4 position-relative">
+		<h2 class="title-label">CURRENT SERIES</h2>
 		<div class="row">
 
 			{{-- comics --}}
 			@foreach ($comics as $item)
-				<div class="col">
-					<div class="comic">
-						<img src="{{ $item['thumb'] }}" alt="">
-						<div class="text-uppercase">{{ $item['series'] }}</div>
-					</div>
+				<div class="col-6 col-md-4 col-lg-2 comic pb-4">
+					<img src="{{ $item['thumb'] }}" alt="" class="mb-2">
+					<div class="text-uppercase">{{ $item['series'] }}</div>
 				</div>
 			@endforeach
 
+		</div>
+		<div class="row justify-content-center load-more">
+			<a href="#" class="w-auto fw-bold">LOAD MORE</a>
 		</div>
 	</div>
 </section>
