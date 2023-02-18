3baa8e12e3c47287458f770157e8982010be0df6
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-02-18 12:41:18 +0100

    index file adapted to layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Laravel Comics</title>
+@extends('templates.app')
 
-		@vite('resources/js/app.js')
-	</head>
-	<body>
-		@include('partials.header')
-		@include('partials.series')
-	</body>
-</html>
+@section('content')
+	@include('partials.series')
+@endsection
