4793c4c4bfc12ba1f81310df9beb1e004a78d4a6
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-02-20 16:55:50 +0100

    search added, top bar added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,10 @@
 <header>
+	<div class="top-bar my-bg-primary">
+		<div class="container px-0 h-100 d-flex align-items-center justify-content-end fw-bold gap-5">
+			<div>DC POWER VISA®</div>
+			<div>ADDICTIONAL DC SITES <i class="fa-solid fa-caret-down ms-1"></i></div>
+		</div>
+	</div>
 	<div class="container px-0">
 		<div class="row">
 			<div class="col py-3"><img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt=""></div>
@@ -10,6 +16,12 @@
 						@endforeach
 					</ul>
 				</nav>
+			</div>
+			<div class="col d-flex align-items-center justify-content-end">
+				<div class="search-bar d-flex align-items-center justify-content-end">
+					<input type="text" class="border-0 text-end me-2 search-input w-100" placeholder="Search">
+					<button type="submit" class="border-0 bg-transparent"><i class="fa-solid fa-magnifying-glass"></i></button>
+				</div>
 			</div>
 		</div>
 	</div>
