d5ace3e6a26437b9e0d8ef5e4a9a4dd38ee5bda6
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-02-20 16:57:50 +0100

    comic detail page added, major style changes

--- resources/views/partials/series.blade.php
+++ resources/views/partials/series.blade.php
@@ -1,13 +1,15 @@
 <section class="series my-bg-dark">
-	<div class="container py-4 position-relative">
-		<h2 class="title-label">CURRENT SERIES</h2>
-		<div class="row">
+	<div class="container px-0 py-4 position-relative">
+		<h4 class="title-label fw-bold">CURRENT SERIES</h4>
+		<div class="row mt-4 pt-4 px-3">
 
 			{{-- comics --}}
-			@foreach ($comics as $item)
-				<div class="col-6 col-md-4 col-lg-2 comic pb-4">
-					<img src="{{ $item['thumb'] }}" alt="" class="mb-2">
-					<div class="text-uppercase">{{ $item['series'] }}</div>
+			@foreach ($comics as $key => $item)
+			<div class="col-6 col-md-4 col-lg-2 comic pb-4">
+				<img src="{{ $item['thumb'] }}" alt="{{ $item['title'] }}" class="mb-2">
+					<a href="{{ route('comic-detail', ['id' => $key]) }}">
+						<div class="text-uppercase">{{ $item['series'] }}</div>
+					</a>
 				</div>
 			@endforeach
 
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,28 @@ Route::get('/', function () {
     $pages = config('comics.pages');
     return view('index', compact('comics', 'pages'));
 });
+
+Route::get('/comic-detail/{id}', function($id) {
+    $comics = config('comics.series');
+    $pages = config('comics.pages');
+    $detail = '';
+    foreach($comics as $key => $item) {
+        if($id == $key) {
+            $detail = $item;
+        }
+    }
+
+    return view('comic-detail', compact('comics', 'pages', 'detail'));
+})->name('comic-detail');
+
+/*
+Route::get('comics/{param}', function ($param) {
+    $comics = config('comics.series');
+
+    $products = array_filter($comics, function($item) use($param) {
+        return $item['titolo'] == $param;
+    });
+
+    return view('comic-detail', compact('products'));
+})->name('detail-product');
+*/
